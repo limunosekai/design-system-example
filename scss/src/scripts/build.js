7aa00476b4b5fc51db006391ca1ce4f9926c1ca0
limunosekai <powerlsh0103@gmail.com>
2022-01-20 23:22:52 +0900

    fix: compile func & test

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -1,17 +1,45 @@
-const fs = require('fs');
-const path = require('path');
+const Fs = require('fs');
+const Path = require('path');
 const sass = require('node-sass');
 
-const result = sass.renderSync({
-    data: fs.readFileSync(
-        path.resolve('src/global.scss'),
+const getComponents = () => {
+  let allComponents = [];
+
+  const types = ['atoms', 'molecules', 'organisms'];
+
+  types.forEach((type) => {
+    const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
+        input: `src/${type}/${file}`,
+        output: `src/lib/${file.slice(0, -4) + 'css'}`
+    }));
+
+    allComponents = [
+        ...allComponents,
+        ...allFiles
+    ];
+  });
+
+  return allComponents;
+}
+
+const compile = (path, filename) => {
+  const result = sass.renderSync({
+    data: Fs.readFileSync(
+        Path.resolve(path),
     ).toString(),
     outputStyle: 'expanded',
     outFile: 'global.css',
-    includePaths: [path.resolve('src')],
-});
+    includePaths: [Path.resolve('src')],
+  });
 
-fs.writeFileSync(
-    path.resolve('src/lib/global.css'),
+  Fs.writeFileSync(
+    Path.resolve(filename),
     result.css.toString()
-);
+  );
+};
+
+compile('src/global.scss', 'src/lib/global.css');
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output)
+});
