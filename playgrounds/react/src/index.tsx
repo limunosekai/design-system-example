2ff7a8525fea4dc4f6aff2a0cc43613ae3ad58a1
limunosekai <powerlsh0103@gmail.com>
2022-01-23 16:22:12 +0900

    setup a react playground

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Button } from 'design-system-react';
+import 'design-system-example/lib/Button.css';
+
+ReactDOM.render(
+    <Button label="example" />,
+    document.querySelector('#root')
+);
