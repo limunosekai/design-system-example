3c8fa606e8f5e56dca8173ce89feaf10b4283f03
limunosekai <powerlsh0103@gmail.com>
2022-01-24 23:19:10 +0900

    Add Select Component

---
+++ packages/react/src/molecules/Select/Select.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Text from '../../atoms/Text';
+
+interface SelectOption {
+  label: string,
+  value: string,
+}
+
+interface SelectProps {
+  onOptionSelected?: (option: SelectOption, optionIndex: number) => void,
+  options?: SelectOption[],
+  label?: string,
+}
+
+const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'Please select one option', onOptionSelected: handler}) => {
+  const labelRef = useRef<HTMLButtonElement>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedIndex, setSelectedIndex] = useState<null | number>(null);
+  const [overlayTop, setOverlayTop] = useState<number>(0);
+
+  const onOptionSelected = (option: SelectOption, optionIndex: number) => {
+    if (handler) {
+      handler(option, optionIndex);
+    }
+
+    setSelectedIndex(optionIndex);
+    setIsOpen(false);
+  }
+
+  const onLabelClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    setOverlayTop((
+      labelRef.current?.offsetHeight || 0
+    ) + 10);
+  }, [labelRef.current?.offsetHeight])
+
+  let selectedOption = null;
+
+  if (selectedIndex !== null) {
+    selectedOption = options[selectedIndex];
+  }
+
+  return (
+    <div className="dse-select">
+      <button ref={labelRef} className="dse-select__label" onClick={() => onLabelClick()}>
+        <Text>{selectedIndex === null ? label : selectedOption?.label}</Text>
+        <svg className={`dse-select__caret ${isOpen ? 'dse-select__caret--open' : 'dse-select__caret--closed'}`} width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M19 9l-7 7-7-7" /></svg>
+      </button>
+
+      {isOpen && (
+        <ul style={{ top: overlayTop }} className="dse-select__overlay">
+          {options.map((option, optionIndex) => {
+            const isSelected = selectedIndex === optionIndex;
+
+            return (
+              <li className={`dse-select__option ${isSelected && 'dse-select__option--selected'}`} onClick={() => onOptionSelected(option, optionIndex)} key={option.value}>
+                <Text>{option.label}</Text>
+                {isSelected && <svg width='1rem' height='1rem' fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M5 13l4 4L19 7" /></svg>}
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </div>
+  )}
+
+export default Select;
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Text, Margin } from 'design-system-react';
+import { Select } from 'design-system-react';
 
 import 'design-system-example/lib/Utilities.css';
 import 'design-system-example/lib/Text.css';
 import 'design-system-example/lib/Margin.css';
+import 'design-system-example/lib/Select.css';
 import 'design-system-example/lib/global.css';
 
+const options = [{
+    label: '1',
+    value: '1',
+}, {
+    label: '2',
+    value: '2',
+}, {
+    label: '3',
+    value: '3'
+}]
+
 ReactDOM.render(
-    <div>
-        <Margin left space="xl">
-            <Text size="xs">something</Text>
-        </Margin>
+    <div style={{ padding: '40px' }}>
+        <Select options={options}/>
     </div>,
     document.querySelector('#root')
 );
