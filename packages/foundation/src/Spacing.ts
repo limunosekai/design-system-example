e62b4fa7cc8f3039c62232f41f8fa39e98bf89ff
limunosekai <powerlsh0103@gmail.com>
2022-01-26 21:22:45 +0900

    Add addon-storysource & addon-knobs

--- packages/foundation/src/Spacing.ts
+++ packages/foundation/src/Spacing.ts
@@ -1,4 +1,15 @@
-const spaces = {
+const spaces: {
+  none: 'none',
+  xxxs: 'xxxs',
+  xxs: 'xxs',
+  xs: 'xs',
+  sm: 'sm',
+  md: 'md',
+  lg: 'lg',
+  xl: 'xl',
+  xxl: 'xxl',
+  xxxl: 'xxxl',
+} = {
   none: 'none',
   xxxs: 'xxxs',
   // 4px
